699173ef8625ecdd96a9f49fd01b5c789e6497c3
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-05-19 21:31:00 -0500

    Added a path to the Task API Endpoint

--- src/services/Endpoint.java
+++ src/services/Endpoint.java
@@ -2,4 +2,5 @@ package services;
 
 public interface Endpoint { //interface for location of different endpoints
     String LOGIN = "http://localhost:7000/api/auth/login";
+    String TASKS = "http://localhost:7000/api/taskItems";
 }
