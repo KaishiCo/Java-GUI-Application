219aa5a0e0e25c9d0d46b23f4decff7598ff8fd8
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-05-19 21:42:46 -0500

    Deleted

--- src/payload/tasks/TaskArrayResponse.java
+++
@@ -1,7 +0,0 @@
-package payload.tasks;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class TaskArrayResponse {    //this class handles the array that is returned from the API's task endpoint
-    Task[] tasks;   //array that will hold tasks
-}
