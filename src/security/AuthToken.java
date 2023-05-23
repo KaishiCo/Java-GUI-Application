e2e969336739d7a4f9ac0bc11fa6844038468450
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-05-23 04:57:15 -0500

    Added a userId field with getters/setters

--- src/security/AuthToken.java
+++ src/security/AuthToken.java
@@ -1,19 +1,29 @@
 package security;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class AuthToken {
     private static String token;
     private static Date expiresIn;
+    private static UUID userId;
 
     public static String getToken() {   //returns the token
         return token;
+    }
+
+    public static UUID getUserId() {
+        return userId;
     }
 
     public static void setAccessToken(String token, long milli) {   //Sets the token and the expiration date
         AuthToken.token = token;
         milli *= 1000;  //API returns number of seconds so this turns it into milliseconds
         expiresIn = new Date(System.currentTimeMillis() + milli);
+    }
+
+    public static void setUserId(UUID id) {
+        userId = id;
     }
 
     public static boolean isExpired() { //returns if the current time is after expiration time
